5ebceed4d5d5efa126accbe2fbbac543167ab070
Reid-Cui7 <reid.cmj@gmail.com>
2022-12-17 09:57:59 +0800

    replace iterrows, which costs most of time

--- data_loader.py
+++ data_loader.py
@@ -7,13 +7,23 @@ from operator import add
 from functools import reduce
 from collections import Counter
 
+def add_with_print(all_corpus):
+    # 程序某一步骤运行很慢时, 可以在那个步骤上加入一个装饰器, 让你知道这一步运行到哪里了, 到底有没有运行
+    add_with_print.i = 0
+
+    def _warp(a, b):
+        add_with_print.i += 1
+        print('{}/{}'.format(add_with_print.i, len(all_corpus)), end=' ')
+        return a + b
+    
+    return _warp
 
 def get_all_vocabulary(train_file_path, vocab_size):
     CUT, SENTENCE = 'cur', 'sentence'
     corpus = pd.read_csv(train_file_path)
     corpus[CUT] = corpus[SENTENCE].apply(lambda s: ' '.join(list(jieba.cut(s))))
     sentence_counters = map(Counter, map(lambda s: s.split(), corpus[CUT].values))
-    chose_words = reduce(add, sentence_counters).most_common(vocab_size)
+    chose_words = reduce(add_with_print(corpus), sentence_counters).most_common(vocab_size)
 
     return [w for w, _ in chose_words]
 
@@ -23,27 +33,24 @@ def tokenizer(sentence, vocab: dict):
 
     return ids
 
-def get_train_data(train_file):
+def get_train_data(train_file, vocab2ids):
     val_ratio = 0.2
-    train_file = 'dataset/train.csv'
     content = pd.read_csv(train_file)
-    X_train, y_train = defaultdict(list), []
-    X_val, y_val = defaultdict(list), []
     num_val = int(len(content) * val_ratio)
+    
+    LABEL, SENTENCE = 'label', 'sentence'
 
-    vocab2ids = None # TODO get embedding based on all vocabularies
-    for i, row in content.iterrows():
-        label = row[1]
-        sentence = row[-1]
-        inputs = tokenizer(sentence, vocab2ids)
+    labels = content[LABEL].values
+    content['input_ids'] = content[SENTENCE].apply(lambda s: ' '.join([str(id_) for id_ in tokenizer(s, vocab2ids)]))
+    sentence_ids = np.array([[int(id_) for id_ in v.split()] for v in content['input_ids'].values])
 
-        if i < num_val:
-            X_val['input_ids'].append(inputs)
-            y_val.append(label)
-        else:
-            X_train['input_ids'].append(inputs)
-            y_train.append(label)
-
+    # shuffle ids
+    ids = np.random.choice(range(len(content)), size=len(content))
+    
+    train_ids = ids[num_val:]
+    val_ids = ids[:num_val]
+    X_train, y_train = sentence_ids[train_ids], labels[train_ids]
+    X_val, y_val = sentence_ids[val_ids], labels[val_ids]
 
     label2id = {label: i for i, label in enumerate(np.unique(y_train))}
     id2label = {i: label for label, i in label2id.items()}
